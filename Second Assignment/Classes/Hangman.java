1dfb7f6a9a3365891f56e09df0ae425c4b3d853b
Momen Ibrahim Fawzy <58600213+Momen-Ibrahim-Fawzy@users.noreply.github.com>
2020-02-27 17:52:50 +0200

    Add files via upload

---
+++ Second Assignment/Classes/Hangman.java
@@ -0,0 +1,145 @@
+package eg.edu.alexu.csd.datastructure.hangman.cs_59;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Hangman implements IHangman{
+    public String[] dictionary;
+    public int maxWrongGuesses;
+    private char[] randomSecretWord,estimaatedSecretWord;
+    private int numOfWrongGuesses=0,numOfRightCharacters=0;
+
+    public String[] readArrayFromFile(String file)throws FileNotFoundException {
+        int count=0;
+        Scanner s1 = null;
+        s1 = new Scanner(new File(file));
+        if (s1==null){
+            throw new FileNotFoundException("thre is no file");
+        }
+        while(s1.hasNextLine())
+        {
+            count++;
+            s1.next();
+        }
+        String[] words = new String[count];
+        s1=new Scanner(new File(file));
+        for (int i =0 ; i<count ; i++)
+        {
+            words[i]=s1.next();
+        }
+        return words;
+    }
+
+    public String getEstimaatedSecretWord(){
+        return String.valueOf(estimaatedSecretWord);
+    }
+
+
+    /**
+     *  Set the dictionary
+     *  @param words: an array of words
+     */
+    public void setDictionary(String[] words){
+        dictionary=words;
+    }
+
+    /**
+     *  Pick a random secret word from the dictionary and returns it
+     *  @return secret word or null
+     */
+    public String selectRandomSecretWord(){
+        int rnd = new Random().nextInt(dictionary.length);
+        randomSecretWord=dictionary[rnd].toCharArray();
+        estimaatedSecretWord=dictionary[rnd].toCharArray();
+        for(int i=0;i<estimaatedSecretWord.length;i++)
+        {
+            estimaatedSecretWord[i]='-';
+        }
+        return dictionary[rnd];
+    }
+
+    /**
+     *  Receive a new user guess, and verify it against the secret word.
+     *  @param c
+     *  case insensitive user guess.
+     *  If c is NULL then ignore it and do no change
+     *  @return * secret word with the remaining hidden characters (use ’-’ instead
+     *  of unsolved characters), or return NULL if user reached max wrong
+     *  guesses.
+     */
+    public String guess(Character c) throws Exception{
+        if (c==null) {
+            return String.valueOf(estimaatedSecretWord);
+        }
+        else
+        {
+            int count = 0;
+            for (int i = 0; i < randomSecretWord.length; i++) {
+                if ((randomSecretWord[i] >= 'a' && randomSecretWord[i] <= 'z') || (randomSecretWord[i] >= 'A' && randomSecretWord[i] <= 'Z')) {
+                    count++;
+                }
+            }
+            if (count != randomSecretWord.length ) {
+                throw new Exception("it is buggy secret word");
+            }
+            else {
+                count = 0;
+                for (int i = 0; i < estimaatedSecretWord.length; i++) {
+                    if (estimaatedSecretWord[i] == Character.toUpperCase(c)||estimaatedSecretWord[i] == Character.toLowerCase(c)) {
+                        count++;
+                    }
+                }
+                if (count!=0)
+                {
+                    System.out.println("this character is taken");
+                    return String.valueOf(estimaatedSecretWord);
+                }
+                else {
+                    count = 0;
+                    for (int i = 0; i < randomSecretWord.length; i++) {
+                        if (randomSecretWord[i] == Character.toUpperCase(c) || randomSecretWord[i] == Character.toLowerCase(c)) {
+                            estimaatedSecretWord[i] = randomSecretWord[i];
+                            count++;
+                        }
+                    }
+                    if (count == 0) {
+                        numOfWrongGuesses++;
+                        System.out.println("wrong choice");
+                    } else {
+                        numOfRightCharacters += count;
+                        System.out.println("right choice");
+                    }
+                    if (numOfWrongGuesses >= maxWrongGuesses) {
+                        System.out.println("you lost");
+                        return null;
+                    }
+                    else {
+                        if (numOfRightCharacters != randomSecretWord.length) {
+                            return String.valueOf(estimaatedSecretWord);
+                        } else {
+                            System.out.println("you win");
+                            return null;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    /**
+     *  Set the maximum number of wrong guesses
+     *  @param max
+     *  maximum number of wrong guesses, If it is NULL, then assume it 1
+     */
+    public void setMaxWrongGuesses(Integer max){
+        //if the integer was negative it will consider max =1 .
+        if(max==null||max<=0){
+            maxWrongGuesses=1;
+        }
+        else {
+            maxWrongGuesses = max;
+        }
+    }
+
+}
---
+++ Second Assignment/Tests/HangmanTest.java
@@ -0,0 +1,103 @@
+package eg.edu.alexu.csd.datastructure.hangman.cs_59;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class HangmanTest {
+
+    @org.junit.jupiter.api.Test
+    void testReadArrayFromFile() throws IOException
+    {
+        Hangman h = new Hangman();
+        String file = "C:\\\\Users\\\\pc\\\\Documents\\\\intellij projects\\\\Hangman Game\\\\src\\\\eg\\\\edu\\\\alexu\\\\csd\\\\datastructure\\\\hangman\\\\cs_59\\\\Google10,000EnglishWord.txt";
+        String[] dictionary = h.readArrayFromFile(file);
+        assertNotNull(dictionary);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetDictionary() throws IOException {
+        Hangman h = new Hangman();
+        String file = "C:\\\\Users\\\\pc\\\\Documents\\\\intellij projects\\\\Hangman Game\\\\src\\\\eg\\\\edu\\\\alexu\\\\csd\\\\datastructure\\\\hangman\\\\cs_59\\\\Google10,000EnglishWord.txt";
+        String[] dictionary = h.readArrayFromFile(file);
+        h.setDictionary(dictionary);
+        assertNotNull(h.dictionary);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSelectRandomSecretWord() throws Exception {
+        Hangman h = new Hangman();
+        String[] dictionary = h.readArrayFromFile("C:\\\\Users\\\\pc\\\\Documents\\\\intellij projects\\\\Hangman Game\\\\src\\\\eg\\\\edu\\\\alexu\\\\csd\\\\datastructure\\\\hangman\\\\cs_59\\\\Google10,000EnglishWord.txt");
+        h.setDictionary(dictionary);
+        String random = h.selectRandomSecretWord();
+        assertNotNull(random);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGuess() throws Exception {
+        //right choice
+        Hangman h = new Hangman();
+        String file = "C:\\\\Users\\\\pc\\\\Documents\\\\intellij projects\\\\Hangman Game\\\\src\\\\eg\\\\edu\\\\alexu\\\\csd\\\\datastructure\\\\hangman\\\\cs_59\\\\Google10,000EnglishWord.txt";
+        String[] dictionary = h.readArrayFromFile(file);
+        h.setDictionary(dictionary);
+        h.setMaxWrongGuesses(4);
+        String random = h.selectRandomSecretWord();
+        assertNotNull(random);
+        int rnd = new Random().nextInt(random.length());
+        assertNotNull(h.guess(random.charAt(rnd)));
+        //taken character
+        assertEquals(h.getEstimaatedSecretWord(),h.guess(random.charAt(rnd)));
+        //return the estimated secret word
+        assertEquals(h.getEstimaatedSecretWord(), h.guess(null));
+        //buggy word
+        Hangman i = new Hangman();
+        file = "C:\\\\Users\\\\pc\\\\Documents\\\\intellij projects\\\\Hangman Game\\\\src\\\\eg\\\\edu\\\\alexu\\\\csd\\\\datastructure\\\\hangman\\\\cs_59\\\\buggy Words.txt";
+        dictionary = i.readArrayFromFile(file);
+        i.setDictionary(dictionary);
+        i.setMaxWrongGuesses(8);
+        String rando = i.selectRandomSecretWord();
+        assertNotNull(random);
+        int rn = new Random().nextInt(random.length());
+        assertThrows(Exception.class, () -> i.guess(rando.charAt(rn)));
+        //wrong choice
+        file = "C:\\\\Users\\\\pc\\\\Documents\\\\intellij projects\\\\Hangman Game\\\\src\\\\eg\\\\edu\\\\alexu\\\\csd\\\\datastructure\\\\hangman\\\\cs_59\\\\chechWrongChar.txt";
+        dictionary = i.readArrayFromFile(file);
+        i.setDictionary(dictionary);
+        i.setMaxWrongGuesses(8);
+        i.selectRandomSecretWord();
+        assertEquals(i.getEstimaatedSecretWord(),i.guess('A'));
+        //loosing
+        i.setMaxWrongGuesses(1);
+        assertEquals(null,i.guess('A'));
+        //winning
+        file = "C:\\\\Users\\\\pc\\\\Documents\\\\intellij projects\\\\Hangman Game\\\\src\\\\eg\\\\edu\\\\alexu\\\\csd\\\\datastructure\\\\hangman\\\\cs_59\\\\checkRightChar.txt";
+        dictionary = i.readArrayFromFile(file);
+        i.setDictionary(dictionary);
+        i.setMaxWrongGuesses(3);
+        random= i.selectRandomSecretWord();
+        assertNotNull(random);
+        //right choice (althogh it is upper case)
+        assertNotNull(i.guess('m'));
+        assertNull(i.guess('o'));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetMaxWrongGuesses() throws Exception{
+        Hangman h = new Hangman();
+        h.setMaxWrongGuesses(4);
+        assertEquals(4,h.maxWrongGuesses);
+        h.setMaxWrongGuesses(0);
+        assertEquals(1,h.maxWrongGuesses);
+        h.setMaxWrongGuesses(null);
+        assertEquals(1,h.maxWrongGuesses);
+        h.setMaxWrongGuesses(-5);
+        assertEquals(1,h.maxWrongGuesses);
+        h.setMaxWrongGuesses(1000);
+        assertEquals(1000,h.maxWrongGuesses);
+    }
+}
